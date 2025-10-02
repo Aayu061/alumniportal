87d201017e61158cbfc08963dc478def04b9bca5
Aayu061 <aayu.panchal61@gmail.com>
2025-10-02 19:52:41 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js - central client config
+// On GH Pages / production set this to your Render backend URL.
+// If you want to test locally, override window.API_BASE in browser devtools.
+window.API_BASE = window.API_BASE || 'https://alumniportal-jg5p.onrender.com';
